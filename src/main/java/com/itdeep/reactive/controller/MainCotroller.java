61405789269403e08b25b27d8a90fa0b95dc8ef6
Akrem AYADI <ayadi.akrem@gmail.com>
2019-07-06 00:47:46 +0200

    Decorate the principle index page

--- src/main/java/com/itdeep/reactive/controller/MainCotroller.java
+++ src/main/java/com/itdeep/reactive/controller/MainCotroller.java
@@ -2,6 +2,7 @@ package com.itdeep.reactive.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,12 +10,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MainCotroller {
 
     @RequestMapping(path = "/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("templateFile", "pages/dashboard");
         return "index";
     }
 
     @GetMapping(path = "/login")
     public String login() {
         return "login";
+    }
+
+    @GetMapping(path = "/logout")
+    public String logout() {
+        return "logout";
     }
 }
