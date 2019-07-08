9868381d2745b9787488e75434b92b6e911d1d78
Akrem AYADI <ayadi.akrem@gmail.com>
2019-07-08 22:56:16 +0200

    Remove Lombok & add the form to create a new user.

--- src/main/java/com/itdeep/reactive/controller/UserController.java
+++ src/main/java/com/itdeep/reactive/controller/UserController.java
@@ -1,39 +1,56 @@
 package com.itdeep.reactive.controller;
 
 import com.itdeep.reactive.entities.User;
+import com.itdeep.reactive.repositories.UserRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.thymeleaf.spring5.context.webflux.IReactiveDataDriverContextVariable;
 import org.thymeleaf.spring5.context.webflux.ReactiveDataDriverContextVariable;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
+import javax.validation.Valid;
 import java.time.Duration;
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
 
+    private final UserRepository userRepository;
 
-    @GetMapping(path = "/")
-    public String home(Model model) {
-        List<User> users = new ArrayList<>();
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
-        users.add(new User("akrem", "ayadi"));
-        users.add(new User("ons", "dridi"));
-        users.add(new User("aisha", "ayadi"));
-        users.add(new User("alex", "devalck"));
+    @GetMapping(path = "")
+    public Mono<String> displayUsers(Model model) {
         //Simulate big list of data, streaming it every 2 second delay
-        Flux<User> userFlux = Flux.fromIterable(users).delayElements(Duration.ofSeconds(2));
+        Flux<User> userFlux = userRepository.findAll().delayElements(Duration.ofSeconds(2));
         IReactiveDataDriverContextVariable reactiveDataDrivenMode =
                 new ReactiveDataDriverContextVariable(userFlux, 1);
-
         model.addAttribute("users", reactiveDataDrivenMode);
-
         model.addAttribute("templateFile", "pages/user");
-        return "index";
+        return Mono.just("index");
+    }
+
+    @GetMapping(path = "/add")
+    public Mono<String> addUser(@ModelAttribute User user, Model model) {
+        model.addAttribute("templateFile", "pages/userform");
+        return Mono.just("index");
+    }
+
+    @PostMapping("/add")
+    Mono<String> signup(@Valid User user, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            return addUser(user, model);
+        }
+        return Mono.just(user)
+                .flatMap(this.userRepository::save)
+                .then(displayUsers(model));
     }
 }
--- src/main/java/com/itdeep/reactive/entities/User.java
+++ src/main/java/com/itdeep/reactive/entities/User.java
@@ -1,21 +1,19 @@
 package com.itdeep.reactive.entities;
 
 
-import lombok.Data;
 import org.springframework.data.annotation.Id;
 
 
 @org.springframework.data.mongodb.core.mapping.Document
-@Data
 public class User {
-
-    public String firstName;
-    public String lastName;
-    public String email;
-    public boolean active;
     @Id
     private String id;
     private String password;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private boolean active;
+
 
     public User() {
     }
@@ -24,5 +22,53 @@ public class User {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = String.format("%s.%s@itdeep.com", firstName, lastName);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
     }
 }
